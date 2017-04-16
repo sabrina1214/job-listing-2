a286f99fe98c4765796aae9eb9309ed8a55a719d
chen <jingchen1214@gmail.com>
2017-04-16 09:31:57 +0800

    admin can see resume list

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   resources :jobs do
     resources :resumes
-  end 
+  end
   root 'jobs#index'
 
   namespace :admin do
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
         post :publish
         post :hide
       end
+
+      resources :resumes
     end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
